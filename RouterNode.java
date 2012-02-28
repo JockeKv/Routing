e4ea5dca0c72e7d44a1827f9246a2632bf34d6de
Joakim Kvarnstroem <joakim.kvarnstroem@gmail.com>
2012-02-28 12:03:28 +0100

    Added untried code

--- RouterNode.java
+++ RouterNode.java
@@ -77,10 +77,29 @@ public class RouterNode {
     }
 
     //--------------------------------------------------
-    public void updateLinkCost(int dest, int newcost) {
-    }
-
-    public void calculateCost(int ngbr, int dest) {
+    public void updateLinkCost(int dest, int newcost, int route) {
 	
     }
+    
+    public int[] min(int[] list) {
+	int[] low = new int[2];
+	low[0] = 999;
+	low[1] = 999;
+	for(int i=0; i<RouterSimulator.NUM_NODES; i++)
+	    if(list[i] < low[0]) {
+		low[0] = list[i];
+		low[1] = i;
+	    }
+	return low;
+    }
+    
+    public int calculateCost(int dest) {
+	int[] list = new int[RouterSimulator.NUM_NODES];
+	for(int i=0; i<RouterSimulator.NUM_NODES; i++)
+	    if(i != myID)
+		list[i] = allCosts[myID][i] + allCosts[i][dest];
+	    else
+		list[i] = allCosts[i][dest];
+	if(min(list)[0] <= allCosts[i][dest])
+	    updateLinkCost(dest, min(list)[0], min(list)[1]);
 }
